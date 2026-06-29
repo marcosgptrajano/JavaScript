521a07c0b166452a963c19a67a64b5bc61ae3708
marcosgptrajano <marcosg.ptrajano@gmail.com>
2026-06-29 18:19:01 -0300

    Condicionais

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 30;
+console.log(`A velocidade do seu carro é ${vel}`);
+if(vel > 40){ 
+    console.log(`Você ultrappassou a velocidade permitida. MULTADO!`);
+}
+console.log('Dirija sempre usando cinto de segurança!');
